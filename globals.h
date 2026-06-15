4c9c3897acedc6e7ad39fcf3652c13270f6e4efb
temex-dev <temexcze@gmail.com>
2026-06-15 08:19:50 +0200

    setup

---
+++ globals.h
@@ -0,0 +1,15 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+const unsigned int DISPLAY_WIDTH = 1920;
+const unsigned int DISPLAY_HEIGHT = 1080;
+const unsigned int WINDOW_SIZE = 800;
+const unsigned int CELL_SIZE = 20;
+const unsigned int GRID_SIZE = WINDOW_SIZE / CELL_SIZE;
+
+#endif // GLOBALS_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "globals.h"
+
 
 int main() {
-    std::cout << "pico";
+    sf::RenderWindow window(sf::VideoMode({ DISPLAY_WIDTH, DISPLAY_HEIGHT }),
+        "Snake",
+        sf::Style::Default,
+        sf::State::Windowed);
+    window.setFramerateLimit(60);
+    window.setPosition({});
+    window.setPosition({ (DISPLAY_WIDTH / 2) - WINDOW_SIZE / 2, (DISPLAY_HEIGHT / 2) - DISPLAY_HEIGHT / 2 });
+
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+
+    bool gameOver = false;
+
+    while (window.isOpen() && !gameOver) {
+
+    }
+
     return 0;
 }
