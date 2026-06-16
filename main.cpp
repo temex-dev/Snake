c04acfaea11efb8f51d81175f0c01ac8b914f57c
temex-dev <temexcze@gmail.com>
2026-06-16 17:58:05 +0200

    uuh something

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "globals.h"
+#include <SFML/Window/WindowEnums.hpp>
 
 
 int main() {
@@ -7,7 +8,6 @@ int main() {
         sf::Style::Default,
         sf::State::Windowed);
     window.setFramerateLimit(60);
-    window.setPosition({});
     window.setPosition({ (DISPLAY_WIDTH / 2) - WINDOW_SIZE / 2, (DISPLAY_HEIGHT / 2) - DISPLAY_HEIGHT / 2 });
 
     sf::ContextSettings settings;
@@ -15,9 +15,21 @@ int main() {
 
     bool gameOver = false;
 
+//main game loop
     while (window.isOpen() && !gameOver) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+                gameOver = true;
+            }
+        }
+        sf::Font font("../assetts/AdwaitaMono-Bold.ttf");
+        sf::Text text(font);
+        text.setString("Hello, World!");
+        window.clear(sf::Color::Black);
+        window.draw(text);
+        window.display();
 
     }
-
     return 0;
 }
